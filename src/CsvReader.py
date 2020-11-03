85d7812f15a3289da7257faf7d609fa13e8f4e0d
Muhammad Zafar <umerzafar85@gmail.com>
2020-11-03 17:26:45 -0500

    Feature: adding travis

--- src/CsvReader.py
+++ src/CsvReader.py
@@ -7,13 +7,17 @@ def ClassFactory(class_name, dictionary):
 
 
 class CsvReader:
+    # setting-up property "data" as a list
     data = []
 
     def __init__(self, filepath):
         self.data = []
-
+        # opening the CSV file as text data
         with open(absolutepath(filepath)) as text_data:
+            # reading the CSV file as dictionary data strucuture
             csv_data = csv.DictReader(text_data, delimiter=',')
+
+            # displaying the contents of the CSV file
             for row in csv_data:
                 self.data.append(row)
         pass
